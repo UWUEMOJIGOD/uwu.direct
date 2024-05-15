2cffe0a240c71eb998302d20442e6b8e69c78801
Unity Chaos <unitylchaos@protonmail.com>
2024-05-14 21:15:49 -0700

    fix broken logo >_<

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -41,7 +41,7 @@ const chainStore = useBlockchain()
   <div class="">
     <div class="flex md:!flex-row flex-col items-center justify-center mb-6 mt-14 gap-2">
       <div class="w-16 rounded-full">
-        <img src="../../assets/uwublack.webp" />
+        
       </div>
       <h1 class="text-primary dark:invert text-3xl md:!text-6xl font-bold">
         {{ $t('pages.title') }}
